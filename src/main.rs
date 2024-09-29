c5571bc40779b0b13103672449f88adfbfc88f95
Devashish Sood <devashishsood9@gmail.com>
2024-09-28 23:35:21 -0400

    feat: vga buffer module and data representations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_std] 
 #![no_main]
-
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World";
 
@@ -15,14 +15,8 @@ pub extern "C" fn _start() -> ! {
     let vga_buffer = 0xb8000 as *mut u8;
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
-            let line_offset: isize = 160 * 24;
-            let char_offset_within_line: isize = i as isize * 2;
-            let color_offset_within_line: isize = i as isize * 2 + 1;
-            let char_offset = char_offset_within_line + line_offset;
-            let color_offset = color_offset_within_line + line_offset;
-
-            *vga_buffer.offset(char_offset) = byte;
-            *vga_buffer.offset(color_offset) = 0xb;
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
 
